55b1cc197702c560aa7449009e67d8a84bd5465e
Takafumi Arakaki <aka.tkf@gmail.com>
2020-06-02 00:11:24 -0700

    Replace: tkf/MicroCollections -> JuliaFolds/MicroCollections

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MicroCollections
 
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://juliafolds.github.io/MicroCollections.jl/dev)
-[![GitHub Actions](https://github.com/tkf/MicroCollections.jl/workflows/Run%20tests/badge.svg)](https://github.com/tkf/MicroCollections.jl/actions?query=workflow%3A%22Run+tests%22)
+[![GitHub Actions](https://github.com/JuliaFolds/MicroCollections.jl/workflows/Run%20tests/badge.svg)](https://github.com/JuliaFolds/MicroCollections.jl/actions?query=workflow%3A%22Run+tests%22)
 
 MicroCollections.jl provides immutable empty and singleton collections.
 
--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,10 @@ makedocs(;
     modules = [MicroCollections],
     format = Documenter.HTML(),
     pages = ["Home" => "index.md", hide("internals.md")],
-    repo = "https://github.com/tkf/MicroCollections.jl/blob/{commit}{path}#L{line}",
+    repo = "https://github.com/JuliaFolds/MicroCollections.jl/blob/{commit}{path}#L{line}",
     sitename = "MicroCollections.jl",
     authors = "Takafumi Arakaki <aka.tkf@gmail.com>",
     strict = true,
 )
 
-deploydocs(; repo = "github.com/tkf/MicroCollections.jl", push_preview = true)
+deploydocs(; repo = "github.com/JuliaFolds/MicroCollections.jl", push_preview = true)
